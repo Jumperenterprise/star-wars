4f2998d9f5eef342a25633a0593d4a1152a22ea9
Jumperenterprise <andrew.jumper.ent@gmail.com>
2021-06-29 19:05:32 +0200

    Limpiando Codigo

--- src/ad.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-    const ad = document.querySelector('publicidad__img')
-
-    let adPosition = window.pageYOffset;
-
-    if(adPosition == 646){
-    ad.style.transform = 'translateY(' + adPosition * .5 + 'px';
-    }
-})
