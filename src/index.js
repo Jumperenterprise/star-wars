b48aa79ca3354ce19bc6452152509cbdc950ab57
Jumperenterprise <andrew.jumper.ent@gmail.com>
2021-06-29 18:07:15 +0200

    Correccion de clases

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles.css'
+import './dropdown'
+import './parallax'
--- src/parallax.js
+++ src/parallax.js
@@ -1,11 +1,9 @@
-
-
 window.addEventListener('scroll', function () {
-  const parallax = document.querySelector('.parallax')
+  const parallax = document.querySelector('.parallaxV')
   const logo = document.querySelector('.logo')
-  let scrollPosition = window.pageYOffset;
+  const img = document.querySelector('.img')
+  let scrollPosition = window.pageYOffset
 
-  parallax.style.transform = 'translateY(' + scrollPosition * .9 + 'px';
-  logo.style.transform = 'translateY(' + scrollPosition * .2 + 'px';
+  parallax.style.transform = 'translateY(' + scrollPosition * 0.9 + 'px'
+  logo.style.transform = 'translateY(' + scrollPosition * -0.3 + 'px'
 })
-
