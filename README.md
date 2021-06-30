d95591cc098b34c7ff476fb41c3269aafa5b23fd
Jumperenterprise <andrew.jumper.ent@gmail.com>
2021-06-30 09:43:12 +0200

    Testing and reparing

---
+++ README.md
@@ -0,0 +1,20 @@
+# Star-Wars
+Project created with HTML, CSS and JS with BEM methodology applying mobile first as RWD method.
+A plugin was created for the menu of mobile and tablet resolutions. We also created a script for the parallax effect and apply various speeds.
+
+### Pre-requisites 📋
+
+Install node_modules to use webpack
+
+## Run Server ⚙️
+
+```
+npx webpack serve
+```
+
+## Autores ✒️
+
+
+* **Andrew ALfaro** 
+
+---
--- src/parallax.js
+++ src/parallax.js
@@ -1,9 +1,10 @@
 window.addEventListener('scroll', function () {
   const parallax = document.querySelector('.parallaxV')
   const logo = document.querySelector('.logo')
-  const img = document.querySelector('.img')
+  const text = document.querySelector('.section__container')
   let scrollPosition = window.pageYOffset
 
   parallax.style.transform = 'translateY(' + scrollPosition * 0.9 + 'px'
   logo.style.transform = 'translateY(' + scrollPosition * -0.3 + 'px'
+  text.style.transform = 'translateY(' + scrollPosition * -0.1 + 'px'
 })
